8bc204e8e8bf9edeae76f2722bf99000ab77b70b
franklinv <franklin.vincent@gmail.com>
2020-09-02 21:16:52 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,9 +11,7 @@ app.get("/products", (req, res) => {
   //const order = req.body.data;
   axios.get('https://eve.theiconic.com.au/catalog/products?gender=female&page=1&page_size=10&sort=popularity').then(resp => {
     
-    console.log(resp.data);
-    
-    res.render('pages/products', {
+      res.render('pages/products', {
       products: resp.data._embedded.product
     });
   });
@@ -25,8 +23,6 @@ app.get("/products", (req, res) => {
 app.get("/productvideo", (req, res) => {
   //const order = req.body.data;
   axios.get('https://eve.theiconic.com.au/catalog/products/LO569SA80GXF/videos').then(resp => {
-    
-    console.log(resp.data._embedded.videos_url[0].url);
     
     res.render('pages/productvideo', {
       videourl: resp.data._embedded.videos_url[0].url
